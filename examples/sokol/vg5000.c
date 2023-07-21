f37acd8a2b68ba3a54e5d380c4fa9761a5cd5177
Sylvain Glaize <mokona@puupuu.org>
2023-07-21 22:50:34 +0200

    vg5000: specify charset data to the system

--- examples/sokol/vg5000.c
+++ examples/sokol/vg5000.c
@@ -88,6 +88,7 @@ vg5000_desc_t vg5000_desc() {
         .roms = {
             .vg5000_10 = { .ptr = dump_vg5000_rom_10, .size = sizeof(dump_vg5000_rom_10) },
             .vg5000_11 = { .ptr = dump_vg5000_rom_11, .size = sizeof(dump_vg5000_rom_11) },
+            .ef9345_charset = { .ptr = dump_vg5000_charset_rom, .size = sizeof(dump_vg5000_charset_rom) },
         },
         #if defined(CHIPS_USE_UI)
         .debug = ui_vg5000_get_debug(&state.ui)
